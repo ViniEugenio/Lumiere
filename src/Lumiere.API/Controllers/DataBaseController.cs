49e5a5d2bd10fa348653e1e9fbc8a3342e265ff6
Vinícius Eugênio <vinicius_eugenio07@hotmail.com>
2026-07-08 18:34:51 -0300

    Database controller

---
+++ src/Lumiere.API/Controllers/DataBaseController.cs
@@ -0,0 +1,17 @@
+﻿using Lumiere.Application.Features.Database.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lumiere.API.Controllers
+{
+    [Route("api/database")]
+    public class DataBaseController(ISender sender) : BaseController(sender)
+    {
+        [HttpPost]
+        public async Task<IActionResult> UpdateDataBase([FromBody] UpdateDataBaseCommand command)
+        {
+            var result = await _sender.Send(command);
+            return Respond(result);
+        }
+    }
+}
